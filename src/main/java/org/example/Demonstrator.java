159d3c7097917f12defdb84bafa9ab7503275879
ajr3-gen <93659748+ajr3-gen@users.noreply.github.com>
2024-01-26 13:13:09 -0500

    Create Demonstrator.java

---
+++ src/main/java/org/example/Demonstrator.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Demonstrator {
+    private String myId;
+    private Integer myInteger;
+    private long myLongPrimitive;
+    private Long myLongObject;
+
+
+    public void setMyId(String in) {
+        this.myId = in;
+    }
+    public String getMyId() {
+        return this.myId;
+    }
+
+    public void setMyInteger(Integer in) {
+        this.myInteger = in;
+    }
+    public Integer getMyInteger() {
+        return this.myInteger;
+    }
+
+    public void setMyLongPrimitive(long in) {
+        this.myLongPrimitive = in;
+    }
+    public long getMyLongPrimitive() {
+        return this.myLongPrimitive;
+    }
+
+    public void setMyLongObject(Long in) {
+        this.myLongObject = in;
+    }
+    public Long getMyLongObject() {
+        return this.myLongObject;
+    }
+}
